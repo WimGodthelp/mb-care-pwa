bb66e0703d181f5653ea18e3d4c583e87108f02f
WimGodthelp <wim@godthelp.com>
2025-09-07 13:20:47 +0200

    redeploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { esmExternals: 'loose' },
-}
-module.exports = nextConfig
+  experimental: { esmExternals: "loose" },
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
+  },
+};
+
+module.exports = nextConfig;
