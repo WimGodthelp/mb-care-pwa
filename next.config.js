c636c17d39f6c771e01982ba49b5454ca16aebc5
WimGodthelp <wim@godthelp.com>
2025-09-12 14:53:46 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,10 @@ const path = require("path");
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
+    // Zorg dat @/... vanaf projectroot resolve’t
     config.resolve.alias["@"] = path.resolve(__dirname);
+    config.resolve.alias["@/components"] = path.resolve(__dirname, "components");
+    config.resolve.alias["@/pages"] = path.resolve(__dirname, "pages");
     return config;
   },
 };
