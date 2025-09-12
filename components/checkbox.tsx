ab14c1b149d4b8d34f84217e4fb7f47ae13c6f60
WimGodthelp <wim@godthelp.com>
2025-09-12 14:28:25 +0200

    Update checkbox.tsx

--- components/checkbox.tsx
+++ components/checkbox.tsx
@@ -1,3 +1,3 @@
-export function Checkbox({ checked, onCheckedChange }:{checked:boolean,onCheckedChange:(v:boolean)=>void}){
-  return <input type='checkbox' checked={checked} onChange={()=>onCheckedChange?.(!checked)} />
+export function Checkbox({ checked, onCheckedChange }:{checked:boolean; onCheckedChange:(v:boolean)=>void}){
+  return <input type="checkbox" checked={checked} onChange={()=>onCheckedChange?.(!checked)} />;
 }
