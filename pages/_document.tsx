1ca8cb90091e4bf44b8ab14ba01dbcc657e6d5d2
WimGodthelp <wim@godthelp.com>
2025-09-12 14:55:07 +0200

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="nl">
       <Head>
-        {/* PWA manifest en kleuren */}
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#F24393" />
-
-        {/* iOS ondersteuning */}
         <link rel="apple-touch-icon" href="/icons/icon-192.png" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
