51c68658c450ad9244ba39e9315498f2726f7dc6
WimGodthelp <wim@godthelp.com>
2025-09-12 14:51:57 +0200

    Update card.tsx

--- components/card.tsx
+++ components/card.tsx
@@ -1,6 +1,1 @@
-export function Card({ className='', children }:{className?:string; children:any}) {
-  return <div className={'border rounded-2xl '+className}>{children}</div>;
-}
-export function CardContent({ className='', children }:{className?:string; children:any}) {
-  return <div className={'p-4 '+className}>{children}</div>;
-}
+
