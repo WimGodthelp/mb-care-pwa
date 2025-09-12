9877f9228f240ecd026b17fb14db0866eaa3c515
WimGodthelp <wim@godthelp.com>
2025-09-12 14:52:16 +0200

    Update button.tsx

--- components/button.tsx
+++ components/button.tsx
@@ -1,5 +1,1 @@
-export function Button({ className='', style, onClick, children }:{
-  className?:string; style?:React.CSSProperties; onClick?:()=>void; children:any
-}) {
-  return <button onClick={onClick} style={style} className={'rounded-xl border px-4 py-2 '+className}>{children}</button>;
-}
+
