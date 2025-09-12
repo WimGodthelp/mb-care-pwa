d6dd092d0e325eb362ff4abc9c24f3be977f70ff
WimGodthelp <wim@godthelp.com>
2025-09-12 14:28:39 +0200

    Update input.tsx

--- components/input.tsx
+++ components/input.tsx
@@ -1,3 +1,3 @@
 export function Input(props:any){
-  return <input {...props} className={'border rounded-xl px-3 py-2 w-full '+(props.className||'')} />
+  return <input {...props} className={'border rounded-xl px-3 py-2 w-full '+(props.className||'')} />;
 }
